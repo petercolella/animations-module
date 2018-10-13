734f1c9fef466c2cc409068115fff7ca3796085f
petercolella <peter.joseph.colella@gmail.com>
2018-10-12 23:58:29 -0400

    npm install

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className="App">
+                <h1>React Animations</h1>
+                <Modal />
+                <Backdrop />
+                <button className="Button">Open Modal</button>
+                <h3>Animating Lists</h3>
+                <List />
+            </div>
+        );
+    }
 }
 
 export default App;
